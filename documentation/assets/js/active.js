6f4d7f2e9f4295760fe3e4534e70ea4cd1cfcd08
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-06-25 02:37:33 -0400

    update

--- documentation/assets/js/active.js
+++
@@ -1,7 +0,0 @@
-    if ($.fn.onePageNav) {
-        $('#doc-nav').onePageNav({
-            currentClass: 'current',
-            scrollSpeed: 500,
-            easing: 'easeOutQuad'
-        });
-    }
--- poca/src/mail.php
+++ poca/src/mail.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "n.islamprivate@gmail.com";
+        $recipient = "davidm989@gmail.com";
 
         // Set the email subject.
         $subject = "New Message from $name";
